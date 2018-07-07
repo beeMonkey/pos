b1302342357d6e53dd379a49c923c0c14912d8ec
AllenKuang <675383749@qq.com>
2018-07-07 17:47:03 +0800

    convert from browser to server

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,217 +1,190 @@
 'use strict';
 
-function printReceipt(tags){
-    //calculateSameItems(tags);
-   let cartItems =buildcartItems(tags);
-    //console.log(sameItems[0].barcode);
-    //console.info(sameItems);
-    let itemDetail=builditemDetail(cartItems);
-    //console.info(detailItems);
+const fixtures = require('../spec/fixtures')
+const { loadAllItems, loadPromotions } = require('../spec/fixtures')
 
-    let receiptItems= buildreceiptItems(itemDetail);
-    //console.info(receiptItems);
+function printReceipt(tags) {
+    let tagWithCounts = buildtagWithCounts(tags);
+    let cartItems = buildicartItems(tagWithCounts);
+    let receiptItems = buildreceiptItems(cartItems);
     calTotalAndSaved(receiptItems);
-    //console.info(JSON.stringify(generateReceipt(receiptItems)));
-    //let formattedBarcodes=buildformattedBarcodes(tags);
-    //setcartItems(formattedBarcodes);
     console.log(generateReceipt(receiptItems));
 };
-
-function buildformattedBarcodes(tags){
-    let formattedBarcodes=[];
-    for(let tag of tags){
-        let  barcodeObject={
-            barcode:tag,
-            count:1
+function buildformattedBarcodes(tags) {
+    return tags.map((tag) => {
+        if (tag.indexOf("-") !== -1) {
+            let [barcode, amount ] = tag.split("-");
+            return {
+                barcode: barcode,
+                amount: parseFloat(amount)
+            }
         }
-        if(tag.indexOf("-")!==-1){
-            let tempArray=tag.split("-");
-            barcodeObject={
-                barcode:tempArray[0],
-                count:parseFloat(tempArray[1])
+        return {
+            barcode: tag,
+            amount: 1
+        }
+    });
+}
+function buildformattedBarcodes(tags) {
+    let formattedBarcodes = [];
+    for (let tag of tags) {
+        let barcodeObject = {
+            barcode: tag,
+            amount: 1
+        }
+        if (tag.indexOf("-") !== -1) {
+            let tempArray = tag.split("-");
+            barcodeObject = {
+                barcode: tempArray[0],
+                amount: parseFloat(tempArray[1])
             }
         }
         formattedBarcodes.push(barcodeObject);
     }
     return formattedBarcodes;
 }
-function setcartItems(formattedBarcodes){
-    console.info(formattedBarcodes);
+// function calTagCount(formattedBarcodes) {
+//     let arr = [];
+//     for (var i = 0; i < formattedBarcodes.length;) {
+//         var count = 0;
+//         for (var j = i; j < formattedBarcodes.length; j++) {
+//             if (formattedBarcodes[i] === formattedBarcodes[j]) {
+//                 formattedBarcodes[i].amount = formattedBarcodes[i].amount + formattedBarcodes[j].amount;
+//             }
+//         }
+
+//         arr.push({
+//             barcode: formattedBarcodes[i],
+//             amount: formattedBarcodes[i].amount 
+//         })
+//         i += count;
+//     }
+//     console.info(arr)
+//     return arr;
+// }
+function calTagCount(formattedBarcodes) {
     let cartItems = [];
-    for(let formattedBarcode of formattedBarcodes){
-        let existCartItem =null;
-        for(let cartItem of cartItems){
-            if(cartItem.barcode===formattedBarcode.barcode)
-            existCartItem = cartItem; 
+    for (let formattedBarcode of formattedBarcodes) {
+        let existCartItem = null;
+        for (let cartItem of cartItems) {
+            if (cartItem.barcode === formattedBarcode.barcode)
+                existCartItem = cartItem;
         }
-        if(existCartItem!=null){
-            existCartItem.count+=formattedBarcode.count;
-        }else{
-            cartItems.push({...formattedBarcode});
+        if (existCartItem != null) {
+            existCartItem.amount += formattedBarcode.amount;
+        } else {
+            cartItems.push({ ...formattedBarcode });
         }
-        console.info(cartItems);
     }
+    return cartItems;
+
+}
+
+function buildtagWithCounts(tags) {
+    let formattedBarcodes = buildformattedBarcodes(tags);
+    let tagWithCounts = calTagCount(formattedBarcodes);
+    return tagWithCounts;
 }
 
 
+// function buildtagWithCounts(tags) {
+//     // key-value
+//     let map = new Map();
+//     // 遍历集合中所有字符串
+//     for (let tag of tags) {
+//         // 判断字符串长度， 如果等于10，直接做统计，否则做特殊处理统计
+//         if (tag.length == 10) {
+//             let ele = tag;
+//             // 判断是否已有存在的key
+//             if (!map.has(ele)) {
+//                 map.set(ele, 0);
+//             }
+//             // 取出value加一
+//             map.set(ele, map.get(ele) + 1);
+//         } else {
+//             // 取字符串第一个字符作为key
+//             let ele = tag.substring(0, 10);
+//             if (!map.has(ele)) {
+//                 map.set(ele, 0);
+//             }
+//             // 取字符串中的数字并做类型转换
+//             let nums = parseFloat(tag.substring(11));
+//             map.set(ele, map.get(ele) + nums);
+//         }
+//     }
+//     let tagWithCounts = [];
+//     map.forEach(function (value, key) {
+//         tagWithCounts.push({
+//             barcode: key,
+//             amount: value
+//         });
+//     })
+//     return tagWithCounts;
+// }
 
 
-function buildcartItems(collection) {
-    // key-value
-    let map = new Map();
-    // 遍历集合中所有字符串
-    for(let i=0;i<collection.length;i++){
-      // 判断字符串长度， 如果等于10，直接做统计，否则做特殊处理统计
-      if(collection[i].length == 10) {
-        let ele = collection[i];
-        // 判断是否已有存在的key
-        if(!map.has(ele)) {
-          map.set(ele, 0);
-        }
-        // 取出value加一
-        map.set(ele,map.get(ele) + 1);
-      } else {
-        // 取字符串第一个字符作为key
-        let ele = collection[i].substring(0,10);
-        if(!map.has(ele)) {
-          map.set(ele, 0);
-        }
-        // 取字符串中的数字并做类型转换
-        let nums= Number(collection[i].substring(11));
-        map.set(ele,map.get(ele) + nums);
-      }
-    }
-    let sameItems=[];
-    map.forEach(function(value,key){
-      sameItems.push({
-        barcode:key,
-        amount:value
-      });
-    })
-    //console.log(arr);
-  return sameItems;
-}
 
-function builditemDetail(sameItems){
-    let detailItems=[];
-    let wholeItems=loadAllItems();
-    // for(let i of sameItems){
-    //     for(let j of wholeItems){
-    //         if(sameItems[i]===wholeItems[j]){
-    //             console.log(wholeItems[j].barcode);
-    //         }
-    //     }
-        
-    // }
-    for(let i=0;i<sameItems.length;i++){
-        for(let j=0;j<wholeItems.length;j++){
-            if(sameItems[i].barcode===wholeItems[j].barcode){
-                    detailItems.push({ 
-                                    barcode:sameItems[i].barcode, 
-                                    name:wholeItems[j].name,
-                                    unit:wholeItems[j].unit,
-                                    price:wholeItems[j].price,
-                                    num:sameItems[i].amount,
-                                    subtotal:sameItems[i].amount*wholeItems[j].price});
-                 }
+function buildicartItems(tagWithCounts) {
+    let detailItems = [];
+    let allItems = fixtures.loadAllItems();
+    for (let tagWithCount of tagWithCounts) {
+        for (let itemOfAll of allItems) {
+            if (tagWithCount.barcode === itemOfAll.barcode) {
+                detailItems.push({
+                    barcode: tagWithCount.barcode,
+                    name: itemOfAll.name,
+                    unit: itemOfAll.unit,
+                    price: itemOfAll.price,
+                    num: tagWithCount.amount,
+                    subtotal: tagWithCount.amount * itemOfAll.price
+                });
+            }
         }
     }
-    //console.log(detailItems);
     return detailItems;
 }
 
-// function discount(detailItems){
-//     let discountItem=loadPromotions();
-//     let discount=0;
-//     for(let i=0;i< discountItem[0].barcodes.length;i++){
-//         for(let j=0; j<detailItems.length;j++){
-//             if(discountItem[0].barcodes[i]===detailItems[j].barcode){
-//                 if(detailItems[j].num>=2){
-//                     detailItems[j].subtotal=detailItems[j].subtotal-detailItems[j].price;
-//                     //detailItems[j].subtotal=detailItems[j].subtotal-(parseInt(detailItems[j].num/3)*2+detailItems[j].num%2);
-//                     discount+=detailItems[j].price;
-//                 }
-//             }
-//         }
-        
-//     }
-//     detailItems.push({discou:discount});         //浅拷贝做法，导致原数组被改变
-//     return detailItems;
-// }
-
-// function discount(detailItems){     //  深拷贝做法，达到效果
-//     let discountItem=loadPromotions();
-//     let discount=0;
-//     let detailItemsClone=detailItems.slice(0);
-//     //console.info(detailItemsClone);
-//     for(let i=0;i< discountItem[0].barcodes.length;i++){
-//         for(let j=0; j<detailItemsClone.length;j++){
-//             if(discountItem[0].barcodes[i]===detailItemsClone[j].barcode){
-//                 if(detailItemsClone[j].num>=2){
-//                     detailItemsClone[j].subtotal=detailItemsClone[j].subtotal-detailItemsClone[j].price;         //满二只减一
-//                     //detailItems[j].subtotal=detailItems[j].subtotal-(parseInt(detailItems[j].num/3)*2+detailItems[j].num%2);
-//                     discount+=detailItemsClone[j].price;
-//                 }
-//             }
-//         }
-        
-//     }
-//     detailItemsClone.push({discou:discount});
-//     return detailItemsClone;
-// }
-function buildreceiptItems(detailItems){     //  深拷贝做法，达到效果
-    let promotions=loadPromotions();
-    //let saved=0;
-    let receipt={};
-    let detailItemsClone=detailItems.slice(0);
-    //console.info(detailItemsClone);
-    for(let i=0;i< promotions[0].barcodes.length;i++){
-        for(let j=0; j<detailItemsClone.length;j++){
-            if(promotions[0].barcodes[i]===detailItems[j].barcode){
-                //if(detailItemsClone[j].num>=2){
-                    detailItemsClone[j].subtotal=detailItemsClone[j].subtotal-detailItemsClone[j].price*(Math.floor(detailItemsClone[j].num/3));    //每满二减一
-                    //detailItems[j].subtotal=detailItems[j].subtotal-(parseInt(detailItems[j].num/3)*2+detailItems[j].num%2);
-                    //saved+=detailItemsClone[j].price*(Math.floor(detailItemsClone[j].num/3));
-                //}
+function buildreceiptItems(cartItems) {     //  深拷贝做法，达到效果
+    let promotions = loadPromotions()[0].barcodes;
+    let receipt = {};
+    let cartItemsClone = cartItems.slice(0);
+    for (let promotion of promotions) {
+        for (let cartItem of cartItemsClone) {
+            if (promotion === cartItem.barcode) {
+                cartItem.subtotal = cartItem.subtotal - cartItem.price * (Math.floor(cartItem.num / 3));    //每满二减一
             }
         }
-        
     }
-    receipt.items=detailItemsClone;
-    //receipt.saved=saved;
-    //detailItemsClone.push({saved:saved});
+    receipt.items = cartItemsClone;
     return receipt;
 }
-function calTotalAndSaved(receipt){
-    let total=0;
-    let saved=0;
-    for(let item of receipt.items){
-        total+=item.subtotal;
-        saved+=item.price*item.num-item.subtotal;
+function calTotalAndSaved(receipt) {
+    let total = 0;
+    let saved = 0;
+    for (let item of receipt.items) {
+        total += item.subtotal;
+        saved += item.price * item.num - item.subtotal;
     }
-    receipt.total=total;
-    receipt.saved=saved;
+    receipt.total = total;
+    receipt.saved = saved;
 
     return receipt;
 }
-function generateReceipt(receipt){
-    //let sum=0;          //数字要记得初始化
-    let title="***<没钱赚商店>收据***\n";
-    let content=title;
-    for(let item of receipt.items){
-        let price=item.price.toFixed(2);
-        //sum=sum+item.subtotal;
-        content+="名称："+item.name+'，'+'数量：'+item.num+item.unit+'，' +
-                 '单价：'+price+'(元)，'+'小计：'+item.subtotal.toFixed(2)+'(元)\n';
+function generateReceipt(receipt) {
+    let content = "***<没钱赚商店>收据***\n";
+    for (let item of receipt.items) {
+        let price = item.price.toFixed(2);
+        content += `名称：${item.name}，数量：${item.num}${item.unit}，单价：${price}(元)，小计：${item.subtotal.toFixed(2)}(元)\n`;
     }
-    // for(let i=0;i<receipt.item.length-1;i++){
-    //     let price=receipt[i].price.toFixed(2);
-
-    //     content+="名称："+receipt[i].name+'，'+'数量：'+receipt[i].num+receipt[i].unit+'，' +
-    //              '单价：'+price+'(元)，'+'小计：'+receipt[i].subtotal.toFixed(2)+'(元)\n';
-    // }
-
-    content+='----------------------\n总计：'+receipt.total.toFixed(2)+'(元)'+'\n';
-    content+='节省：'+receipt.saved.toFixed(2)+'(元)'+'\n**********************';
+    content += `----------------------\n总计：${receipt.total.toFixed(2)}(元)\n`;
+    content += `节省：${receipt.saved.toFixed(2)}(元)\n**********************`;
     return content;
+}
+
+module.exports = {
+    buildtagWithCounts,
+    buildicartItems,
+    buildreceiptItems,
+    calTotalAndSaved,
+    printReceipt
 }
---
+++ pos_v1/spec/main-spec.js
@@ -0,0 +1,137 @@
+'use stricst'
+
+const main = require('../main/main')
+const { buildtagWithCounts, buildicartItems, buildreceiptItems, calTotalAndSaved, printReceipt } = require('../main/main')
+describe('main().buildtagWithCounts(tags)-test', () => {
+    it('should return  Array', () => {
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+
+        const cartItems = JSON.stringify(main.buildtagWithCounts(tags));
+
+        const expectArray = JSON.stringify([
+            { barcode: "ITEM000001", amount: 5 },
+            { barcode: "ITEM000003", amount: 2.5 },
+            { barcode: "ITEM000005", amount: 3 }]);
+        expect(cartItems).toBe(expectArray);
+    });
+});
+
+describe('main().buildcartItems()-test', () => {
+    it('should return  Array', () => {
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+        const tagWithCounts = main.buildtagWithCounts(tags);
+
+        const cartItems = JSON.stringify(main.buildicartItems(tagWithCounts));
+        //const detailItems=5;
+        const expectArray = JSON.stringify([
+            {
+                barcode: "ITEM000001", name: "雪碧", unit: "瓶", price: 3, num: 5, subtotal: 15
+            },
+            {
+                barcode: "ITEM000003", name: "荔枝", unit: "斤", price: 15, num: 2.5, subtotal: 37.5
+            },
+            { barcode: "ITEM000005", name: "方便面", unit: "袋", price: 4.5, num: 3, subtotal: 13.5 },
+        ]);
+        expect(cartItems).toBe(expectArray);
+    });
+});
+
+describe('main().buildreceiptItems-test', () => {
+    it('should return  Array', () => {
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+        const tagWithCounts = buildtagWithCounts(tags);
+        const cartItems = buildicartItems(tagWithCounts);
+
+        const receipItems = JSON.stringify(buildreceiptItems(cartItems));
+
+        const expectArray = JSON.stringify({ "items": [{ "barcode": "ITEM000001", "name": "雪碧", "unit": "瓶", "price": 3, "num": 5, "subtotal": 12 }, { "barcode": "ITEM000003", "name": "荔枝", "unit": "斤", "price": 15, "num": 2.5, "subtotal": 37.5 }, { "barcode": "ITEM000005", "name": "方便面", "unit": "袋", "price": 4.5, "num": 3, "subtotal": 9 }] });
+        expect(receipItems).toBe(expectArray);
+    });
+});
+
+describe('main().calTotalAndSaved-test', () => {
+    it('should get  total and saved', () => {
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+        const tagWithCounts = buildtagWithCounts(tags);
+        const cartItems = buildicartItems(tagWithCounts);
+
+        const receipItems = buildreceiptItems(cartItems);
+
+        const totalAndSaved = calTotalAndSaved(receipItems);
+
+        const expectTotal = 58.50;
+        const expectSaved = 7.50;
+        expect(totalAndSaved.total).toBe(expectTotal);
+        expect(totalAndSaved.saved).toBe(expectSaved);
+    });
+});
+
+
+describe('pos', () => {
+
+    it('should print text', () => {
+  
+      const tags = [
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000003-2.5',
+        'ITEM000005',
+        'ITEM000005-2',
+      ];
+  
+      spyOn(console, 'log');
+  
+      printReceipt(tags);
+  
+      const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+    expect(console.log).toHaveBeenCalledWith(expectText);
+  });
+});
+
+  
